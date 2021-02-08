5480880cf02623db5dd2862d20ac50fbc30e0b28
spm <spmcginnis@users.noreply.github.com>
2021-02-07 22:57:46 -0600

    refactored ImageSet into its own class

--- Assets/Scripts/SceneElements/Character.cs
+++ Assets/Scripts/SceneElements/Character.cs
@@ -7,7 +7,7 @@ namespace PopKuru
     public class Character
     {
         public string Name { get; private set; }
-        public List<ImageSet> PoseSets = new List<ImageSet>();
+        public List<ImageSet> ImageSetList = new List<ImageSet>();
 
         public Character(string name)
         {
@@ -15,39 +15,10 @@ namespace PopKuru
             this.Name = name;
 
             // Initialize Image Index for that name
-            InitializeImageSets(name);
+            ImageSetList = ImageSet.InitializeImageSets(name);
 
-            Debug.Log($"Character object for {this.Name} initialized. {this.PoseSets[0].IndexOf} image set: {this.PoseSets[0].Body}, {this.PoseSets[0].FaceList["smile"]}.");
+            Debug.Log($"Character object for {this.Name} initialized. {this.ImageSetList[0].IndexOf} image set: {this.ImageSetList[0].Body}, {this.ImageSetList[0].FaceList["smile"]}.");
         }
-
-        public class ImageSet 
-        {
-            public string IndexOf { get; private set;}
-            public string Body { get; private set; }
-            public Dictionary<string, string> FaceList { get; private set; }
-
-            public ImageSet(string bodyURL, Dictionary<string, string> faceList, string indexOf = "default image" )
-            {
-                this.IndexOf = indexOf;
-                this.Body = bodyURL;
-                this.FaceList = faceList;
-            }
-        }
-
-        private void InitializeImageSets(string name)
-        {
-          switch (name.ToLower())
-            {
-                case "margot":
-                    string body1 = "test body URL";
-                    Dictionary<string, string> faceList1 = new Dictionary<string, string>()
-                    {
-                        {"smile", "test smile URL"}
-                    };
-                    string poseName = "Standard Pose";
-                    PoseSets.Add(new ImageSet(bodyURL:body1, faceList:faceList1, indexOf:poseName));
-                    break;
-            }
-        }
+ 
     }
 }
---
+++ Assets/Scripts/SceneElements/ImageSet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PopKuru
+{
+    public class ImageSet
+    {
+        public string IndexOf { get; private set;}
+        public string Body { get; private set; }
+        public Dictionary<string, string> FaceList { get; private set; }
+
+        public ImageSet(string bodyURL, Dictionary<string, string> faceList, string indexOf = "default image" )
+        {
+            this.IndexOf = indexOf;
+            this.Body = bodyURL;
+            this.FaceList = faceList;
+        }
+
+       public static List<ImageSet> InitializeImageSets(string name)
+        {
+            List<ImageSet> ImageSetList = new List<ImageSet>();
+            switch (name.ToLower())
+            {
+                case "margot":
+                    string body1 = "test body URL";
+                    Dictionary<string, string> faceList1 = new Dictionary<string, string>()
+                    {
+                        {"smile", "test smile URL"}
+                        // Add more pairs for the expressions that go with that same body pose
+                    };
+                    string poseName = "Standard Pose";
+                    ImageSetList.Add(new ImageSet(bodyURL:body1, faceList:faceList1, indexOf:poseName));
+                    break;
+                // default: create a blank character image set for default fallback and testing purposes.  silhoutte with question mark face.
+                
+            }
+
+            return ImageSetList;
+        }
+
+
+    }
+}
